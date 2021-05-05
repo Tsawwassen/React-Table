0e6408dd0dac559920c0c7edff6e343570758e99
Mitchell <mitchell.rian.smith@gmail.com>
2021-05-05 09:36:12 -0700

    Add component folder to React client

---
+++ client/src/components/Table.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+
+
+class Table extends Component {
+    
+  constructor(props){
+    super(props);
+    this.state = {
+        
+
+    }
+ 
+  }
+
+
+
+  render () {
+    return (
+      <h3>Table Component</h3>
+      
+    );
+  }
+}
+
+export default Table;
