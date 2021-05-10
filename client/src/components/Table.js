a96147523d580bf07fcdfe30a33aa2daa089f0e4
Mitchell <mitchell.rian.smith@gmail.com>
2021-05-10 08:26:46 -0700

    Display basic table, display headers from database data keys

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -8,27 +8,58 @@ class Table extends Component {
   constructor(props){
     super(props);
     this.state = {
-        table:[]
+        table:[],
+        headers:[]
 
     }
-    console.log("hello");
+
+    this.testButton = this.testButton.bind(this);
+    this.displayHeader = this.displayHeader.bind(this);
+  }
+  /* Helper function to remove _id key */
+  removeIDKey(item){
+    //console.log(`inside removeIDKEy function ${array}`);
+    let t_headers = Object.keys(item);
+    t_headers.shift();
+    return t_headers;
   }
 
   componentDidMount(){
     fetch('/api/table')
     .then(res => res.json())
      .then(data => {
-       console.log(data);
-       //this.setState(table:data.table)
+       this.setState( {table:data.data, headers: this.removeIDKey(data.data[0])} );
      });
     
   }
 
+  testButton(){
+    console.table(this.state.table);
+    console.log(this.state.headers);
+  }
+
+  displayHeader(header, index){
+    return (<th key={index}>{header}</th>)
+  }
 
   render () {
     return (
-      <h3>Table Component</h3>
-      
+      <div>
+        <h3>Table Component</h3>
+        <table>
+          <thead>
+            <tr>
+              {this.state.headers.map( (header, index) => this.displayHeader(header, index) )}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td><td>2</td><td>3</td><td>4</td>
+            </tr>
+          </tbody>
+        </table>
+        <button onClick={this.testButton}>Test Button</button>
+      </div>
     );
   }
 }
